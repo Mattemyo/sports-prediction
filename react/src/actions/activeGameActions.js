4768b9bf090321e897bd8e4d279e1afa51d8e1ec
Mattemyo <matias97marti@gmail.com>
2018-04-21 18:34:53 +0200

    set up request for home and away team info

--- react/src/actions/activeGameActions.js
+++ react/src/actions/activeGameActions.js
@@ -3,7 +3,9 @@ import {
   LIVE_GAMES_FETCHED,
   ACTIVE_GAME_UPDATED,
   ACTIVE_GAME_DETAILS_FETCHED,
-  ACTIVE_GAME_PREDICTION_FETCHED
+  ACTIVE_GAME_PREDICTION_FETCHED,
+  HOME_TEAM_DETAILS_FETCHED,
+  AWAY_TEAM_DETAILS_FETCHED
 } from '../actionTypes';
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -22,6 +24,26 @@ export const fetchActiveGameDetails = (gameId) => (dispatch) =>
   api.liveGames
     .fetchActiveGameDetails(gameId)
     .then((data) => dispatch(activeGameDetailsFetched(data)));
+
+export const homeTeamDetailsFetched = (data) => ({
+  type: HOME_TEAM_DETAILS_FETCHED,
+  homeTeamDetails: data
+});
+
+export const fetchHomeTeamDetails = (homeTeamId) => (dispatch) =>
+  api.liveGames
+    .fetchHomeTeamDetails(homeTeamId)
+    .then((data) => dispatch(homeTeamDetailsFetched(data)));
+
+export const awayTeamDetailsFetched = (data) => ({
+  type: AWAY_TEAM_DETAILS_FETCHED,
+  awayTeamDetails: data
+});
+
+export const fetchAwayTeamDetails = (awayTeamId) => (dispatch) =>
+  api.liveGames
+    .fetchAwayTeamDetails(awayTeamId)
+    .then((data) => dispatch(awayTeamDetailsFetched(data)));
 
 export const activeGamePredictionFetched = (data) => ({
   type: ACTIVE_GAME_PREDICTION_FETCHED,
--- react/src/reducers/activeGameReducer.js
+++ react/src/reducers/activeGameReducer.js
@@ -1,7 +1,9 @@
 import {
   ACTIVE_GAME_UPDATED,
   ACTIVE_GAME_DETAILS_FETCHED,
-  ACTIVE_GAME_PREDICTION_FETCHED
+  ACTIVE_GAME_PREDICTION_FETCHED,
+  AWAY_TEAM_DETAILS_FETCHED,
+  HOME_TEAM_DETAILS_FETCHED
 } from '../actionTypes';
 
 export default (state = {}, action = {}) => {
@@ -11,6 +13,12 @@ export default (state = {}, action = {}) => {
 
     case ACTIVE_GAME_DETAILS_FETCHED:
       return { ...state, ...action.activeGameDetails };
+
+    case HOME_TEAM_DETAILS_FETCHED:
+      return { ...state, homeTeamDetails: action.homeTeamDetails };
+
+    case AWAY_TEAM_DETAILS_FETCHED:
+      return { ...state, awayTeamDetails: action.awayTeamDetails };
 
     case ACTIVE_GAME_PREDICTION_FETCHED:
       return { ...state, activeGamePrediction: action.activeGamePrediction };
