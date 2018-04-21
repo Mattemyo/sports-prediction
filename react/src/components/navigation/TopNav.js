61f539a04a2349f5d54a8fdb32e33a3f8faa22a2
Mattemyo <matias97marti@gmail.com>
2018-04-21 14:51:55 +0200

    style layout of desktop nav

--- react/src/components/navigation/TopNav.js
+++ react/src/components/navigation/TopNav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './TopNav.css';
 
 export default () => (
   <nav>
     <div className="nav-logo">
       <h3>&#9917; and FutureScore</h3>
+    </div>
+    <div className="nav-links">
+      <a href="http://#">Competitions</a>
+      <a href="http://#">About</a>
+      <a href="http://#">Cool Facts</a>
     </div>
   </nav>
 );
