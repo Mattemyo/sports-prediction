91efa4f36b3d9e0091685bd5da42a8b7546c8b78
Mattemyo <matias97marti@gmail.com>
2018-04-21 15:54:45 +0200

    make navbar responsive

--- react/src/components/navigation/TopNav.js
+++ react/src/components/navigation/TopNav.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TopNav.css';
 
-export default () => (
-  <nav>
-    <div className="nav-logo">
-      <h3>&#9917; and FutureScore</h3>
-    </div>
-    <div className="nav-links">
-      <a href="http://#">Competitions</a>
-      <a href="http://#">About</a>
-      <a href="http://#">Cool Facts</a>
-    </div>
-  </nav>
-);
+export default class TopNav extends Component {
+  state = {
+    open: true
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <nav>
+        <div className="nav-logo">
+          <h3>&#9917; and FutureScore</h3>
+        </div>
+        <div className={`nav-links ${open && 'hidden'}`}>
+          <a href="http://#">Competitions</a>
+          <a href="http://#">About</a>
+          <a href="http://#">Cool Facts</a>
+        </div>
+        <button id="hamburger" onClick={() => this.setState({ open: !open })}>
+          <i className={`fas fa-bars fa-2x ${!open && 'hidden'}`} />
+          <i className={`fas fa-times fa-2x ${open && 'hidden'}`} />
+        </button>
+      </nav>
+    );
+  }
+}
