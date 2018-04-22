e0643171c8161b49fccba8f3b2ff1891dccd4310
Mattemyo <matias97marti@gmail.com>
2018-04-22 22:04:21 +0200

    style league table

--- react/src/components/tables/LeagueTable.js
+++ react/src/components/tables/LeagueTable.js
@@ -5,17 +5,31 @@ import './LeagueTable.css';
 const LeagueTable = ({ loading, activeCompetition }) => (
   <div className="table grid">
     <h2>{!loading && activeCompetition.table.leagueCaption}</h2>
-    {!loading &&
-      activeCompetition.table.standing.map((team) => (
-        <div key={team}>
-          <h4>{team.position}</h4>
-          <h4>{team.teamName}</h4>
-          <img src={team.crestURI} alt={team.teamName} />
-          <h4>{team.playedGames}</h4>
-          <h4>{team.goalDifference}</h4>
-          <h4>{team.points}</h4>
-        </div>
-      ))}
+    <div className="league-row labels grid">
+      <h3>#</h3>
+      <h3 className="spacer" />
+      <h3>Team</h3>
+      <h3>GP</h3>
+      <h3>+ / -</h3>
+      <h3>P</h3>
+    </div>
+    <div className="standing grid">
+      {!loading &&
+        activeCompetition.table.standing.map((team) => (
+          <div className="listed-team league-row grid" key={team.teamName}>
+            <h1>{team.position}</h1>
+            <div className="img-container-small">
+              <img src={team.crestURI} alt={team.teamName} />
+            </div>
+            <h4 className="team-name">{team.teamName}</h4>
+            <h4 className="played-games">{team.playedGames}</h4>
+            <h4 className="goal-diff">{team.goalDifference}</h4>
+            <h4 className="points">
+              <strong>{team.points}</strong>
+            </h4>
+          </div>
+        ))}
+    </div>
   </div>
 );
 
