c2302bf3f72aadf9a76f6906811b3ff7c7c9f38d
Mattemyo <matias97marti@gmail.com>
2018-04-29 23:43:23 +0200

    change welcome text

--- native/App.js
+++ native/App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Soccer Predicition App</Text>
+        <Text>This Application will predict the outcome of soccer games.</Text>
+        <Text>This </Text>
       </View>
     );
   }
